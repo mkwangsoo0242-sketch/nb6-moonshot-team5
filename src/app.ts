f95d24ab3b07f022be3895e2fc85b4bb7a9540a4
Yeongjun <ing08114@gmail.com>
2025-12-23 10:48:02 +0900

    프로젝트 베이스

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import { PORT } from './lib/constants';
+
+//express app 생성
+const app = express();
+
+//CORS 설정
+app.use(cors());
+
+//JSON 파싱 미들웨어 설정
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+//서버 시작
+app.listen(PORT, () => {
+  console.log(`team5 Server is running on port ${PORT}`);
+});
